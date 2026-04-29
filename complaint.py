fd39a0bea622486bfb6cdbbef89b4b40886298b3
Mitva <mitvasatani@gmail.com>
2026-04-30 00:21:18 +0530

    image feature added

---
+++ complaint.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Float, Integer, String
+from database.database import Base
+
+class Complaint(Base):
+    __tablename__ = "complaints"
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String)
+    category = Column(String)
+    priority = Column(String)
+    status = Column(String, default="Pending")
+    lat = Column(Float, nullable=True)
+    long = Column(Float, nullable=True)
+    image_path = Column(String, nullable=True)
